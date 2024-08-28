9f116a1a64bfea410673ac3b620f114d0617f684
Mohamed Sabry <mohamed.sabry@lyveglobal.com>
2024-08-28 16:57:16 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const broadCast = new BroadcastChannel('number');
+
+
+
+
+let FireRonaomNumber =()=>{
+    broadCast.postMessage({
+        number: Math.random() * 100,
+    });
+}
+
+broadCast.onmessage = ({ data = {} }) => {
+    console.log(data,"TESSt");
+};
